f16b649bf126d38dcda3c6881291d7670d37f24e
veena56 <veena.h@promantia.com>
2021-12-23 16:18:52 +0530

    10781,10783/bugfix

--- naked_manufacturing/naked_manufacturing/doctype/item/item.js
+++ naked_manufacturing/naked_manufacturing/doctype/item/item.js
@@ -4,13 +4,17 @@ frappe.ui.form.on('Item', {
 	},
 	before_save: function (frm) {
 		validate_min_max_value(frm)
+		validate_unique_fiber_type(frm.doc.fiber_type_details)
 		validate_fiber_percentage(frm.doc.fiber_type_details)
 		validate_total_fiber_percentage(frm.doc.fiber_type_details)
 	}
 })
 
-frappe.ui.form.on('Product Details', {
-	fiber_:function(frm,cdt,cdn){
+frappe.ui.form.on('Fiber Type Details', {
+	fiber_type: function (frm,cdt,cdn) {
+	validate_unique_fiber_type(frm.doc.fiber_type_details)	
+	},
+	fiber_: function (frm, cdt, cdn) {
 		validate_fiber_percentage(frm.doc.fiber_type_details)
 	}
 })
@@ -18,26 +22,35 @@ frappe.ui.form.on('Product Details', {
 function validate_min_max_value(frm) {
 	if (frm.doc.gsm_field < 0 || frm.doc.gsm_field > 1000) {
 		frappe.validated = false;
-		msgprint("GSM field value should be greater than 0 and less than 1000.")
+		msgprint("GSM field value should be between 0 to 1000.")
 	}
 }
 
-function validate_fiber_percentage(items){
-	$.each(items,function(idx,item){
-		if(item.fiber_<0 ||item.fiber_>100){
+function validate_fiber_percentage(items) {
+	$.each(items, function (idx, item) {
+		if (item.fiber_ < 0 || item.fiber_ > 100) {
 			frappe.validated = false;
 			msgprint("Fiber percentage should be greater than 0 and less than 100")
 		}
 	});
 }
-function validate_total_fiber_percentage(items){
-	var sum_fiber_percent=0
-	$.each(items,function(idx,item){
-		console.log(item.fiber_)
-		sum_fiber_percent+=item.fiber_
+function validate_total_fiber_percentage(items) {
+	var sum_fiber_percent = 0
+	$.each(items, function (idx, item) {
+		sum_fiber_percent += item.fiber_
 	})
-	if(sum_fiber_percent!=100){
+	if (items.length > 0 && sum_fiber_percent != 100) {
 		frappe.validated = false;
 		msgprint("Total Fiber Percentage should be equal to 100")
+	}
+}
+
+function validate_unique_fiber_type(item){
+	for (var i = 0; i < item.length; i++) {
+		for (var j = i + 1; j < item.length; j++) {
+			if (item[i].fiber_type == item[j].fiber_type) {
+				frappe.throw(__("Same Fiber Type should not be allowed to added twice." + item[j].fiber_type));
+			}
+		}
 	}
 }

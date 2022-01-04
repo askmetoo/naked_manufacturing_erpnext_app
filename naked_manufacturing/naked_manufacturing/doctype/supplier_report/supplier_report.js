15a4f758d090b5eb535a6e927677b35fad0f4615
veena56 <veena.h@promantia.com>
2022-01-04 12:20:15 +0530

    10783/bugfix

--- naked_manufacturing/naked_manufacturing/doctype/supplier_report/supplier_report.js
+++ naked_manufacturing/naked_manufacturing/doctype/supplier_report/supplier_report.js
@@ -3,6 +3,17 @@
 
 frappe.ui.form.on('Supplier Report', {
 	modified_report: function (frm) {
+		if(frm.doc.modified_report){
+			var modified=frm.doc.modified_report.lastIndexOf(".");  
+			var original=frm.doc.original_report.lastIndexOf("."); 
+			if((frm.doc.modified_report.slice(modified + 1))!=(frm.doc.original_report.slice(original + 1))){
+				frappe.validated = false;
+				frm.doc.modified_report=''
+				frm.refresh_field('modified_report')
+				msgprint("Original report and Modified report file extension should be same.")
+			}
+
+		}
 		if(frm.doc.modified_report!=""){
 			frm.doc.status="Processed"
 			frm.refresh_field("status")

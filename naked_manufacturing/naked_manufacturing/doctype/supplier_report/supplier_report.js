1f7677bf416ad355453acace39e292b5bcee1d66
veena56 <veena.h@promantia.com>
2022-01-05 12:49:40 +0530

    10836/bugfix

--- naked_manufacturing/naked_manufacturing/doctype/supplier_report/supplier_report.js
+++ naked_manufacturing/naked_manufacturing/doctype/supplier_report/supplier_report.js
@@ -3,20 +3,23 @@
 
 frappe.ui.form.on('Supplier Report', {
 	modified_report: function (frm) {
-		if(frm.doc.modified_report){
-			var modified=frm.doc.modified_report.lastIndexOf(".");  
-			var original=frm.doc.original_report.lastIndexOf("."); 
-			if((frm.doc.modified_report.slice(modified + 1))!=(frm.doc.original_report.slice(original + 1))){
-				frappe.validated = false;
-				frm.doc.modified_report=''
-				frm.refresh_field('modified_report')
-				msgprint("Original report and Modified report file extension should be same.")
-			}
-
+		if(frm.doc.modified_report!=""||frm.doc.modified_report!=undefined){
+			frm.doc.doc_status="Processed"
+			frm.refresh_field("doc_status")
 		}
-		if(frm.doc.modified_report!=""){
-			frm.doc.status="Processed"
-			frm.refresh_field("status")
+	},
+	before_submit:function(frm){
+		if((frm.doc.modified_report==""||frm.doc.modified_report==undefined)&&(frm.doc.original_report==""||frm.doc.original_report==undefined)){
+			frappe.validated = false;
+			msgprint("Original report and Modified report are mandatory")
+		}
+		else if(frm.doc.modified_report==""||frm.doc.modified_report==undefined){
+			frappe.validated = false;
+			msgprint("Modified report is mandatory")
+		}
+		else if(frm.doc.original_report==""||frm.doc.original_report==undefined){
+			frappe.validated = false;
+			msgprint("Original report is mandatory")
 		}
 	}
 });

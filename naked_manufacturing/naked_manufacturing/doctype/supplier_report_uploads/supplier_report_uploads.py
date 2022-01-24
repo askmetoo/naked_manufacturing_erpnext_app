96cd1d7ec6c2df04a639f57a882b50b48862cf74
veena56 <veena.h@promantia.com>
2022-01-24 16:52:22 +0530

    10927/rename the doctype

--- naked_manufacturing/naked_manufacturing/doctype/supplier_report_uploads/supplier_report_uploads.py
+++ naked_manufacturing/naked_manufacturing/doctype/supplier_report_uploads/supplier_report_uploads.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2021, admin@gmail.com and contributors
+# Copyright (c) 2022, admin@gmail.com and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
-class SupplierReport(Document):
+class SupplierReportUploads(Document):
 	pass

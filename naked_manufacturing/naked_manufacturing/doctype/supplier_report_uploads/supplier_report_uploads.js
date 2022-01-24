e2b6cb825ed6c208597f24a7730b4a79099a46c4
veena56 <veena.h@promantia.com>
2022-01-24 15:12:44 +0530

    10930/feedback given by zack

--- naked_manufacturing/naked_manufacturing/doctype/supplier_report_uploads/supplier_report_uploads.js
+++ naked_manufacturing/naked_manufacturing/doctype/supplier_report_uploads/supplier_report_uploads.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, admin@gmail.com and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Supplier Products", {
+frappe.ui.form.on("Supplier Report Uploads", {
     "supplier_id": function(frm) {
     	frappe.db.get_value("Supplier", frm.doc.supplier_id, "product_lookup",(s)=>{
 		frm.set_value('product_identifier',s.product_lookup) 
